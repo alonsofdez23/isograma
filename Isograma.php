fe0c7f6a10f67d6a684b3c97ad741f751f7d3f79
Alonso Fernández Vidal <alonsobs23@gmail.com>
2020-10-16 01:00:51 +0200

    Resuelto

--- Isograma.php
+++ Isograma.php
@@ -1,13 +1,19 @@
 <?php
 
-/*
- * Esto es sólo un ESQUELETO para el ejercicio de los "isogramas".
+/**
+ * @author Alonso Fernández Vidal
+ * @copyright Copyright (c) 2020 Alonso Fernández
+ * @license https://www.gnu.org/licenses/gpl-3.0.txt
  */
 
+/**
+ * esIsograma
+ *
+ * @param string $s
+ * @return void
+ */
 function esIsograma($s)
 {
-    //
-    // TU CÓDIGO VA AQUÍ
-    //
+    $s = mb_str_split(mb_ereg_replace('[- ]', '', mb_strtolower($s)));
+    return array_unique($s) == $s;
 }
-
